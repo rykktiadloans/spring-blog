6c573533ef505f65080b1eb9a1bd16e899123d47
rykktiadloans <datokar1705@gmail.com>
2025-07-01 20:14:49 +0200

    added HTTPS support for the gateway

--- gateway/src/main/java/org/rl/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/org/rl/gateway/config/GatewayConfig.java
@@ -1,19 +1,32 @@
 package org.rl.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.rl.gateway.filters.SecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.reactive.WebFluxProperties;
+import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 import java.beans.Customizer;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @Configuration
+@Slf4j
 public class GatewayConfig {
     @Value("${services.api}")
     private String apiUrl;
@@ -42,4 +55,37 @@ public class GatewayConfig {
                         .uri(frontendUrl))
                 .build();
     }
+
+    @Bean
+    public WebFilter httpsRedirectFilter() {
+        return new WebFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                URI originalUri = exchange.getRequest().getURI();
+                log.debug("Hello from filter!");
+
+                //here set your condition to http->https redirect
+                List<String> forwardedValues = exchange.getRequest().getHeaders().get("x-forwarded-proto");
+                if (forwardedValues != null && forwardedValues.contains("http")) {
+                    try {
+                        URI mutatedUri = new URI("https",
+                                originalUri.getUserInfo(),
+                                originalUri.getHost(),
+                                originalUri.getPort(),
+                                originalUri.getPath(),
+                                originalUri.getQuery(),
+                                originalUri.getFragment());
+                        ServerHttpResponse response = exchange.getResponse();
+                        response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+                        response.getHeaders().setLocation(mutatedUri);
+                        return Mono.empty();
+                    } catch (URISyntaxException e) {
+                        throw new IllegalStateException(e.getMessage(), e);
+                    }
+                }
+                return chain.filter(exchange);
+            }
+        };
+    }
+
 }
